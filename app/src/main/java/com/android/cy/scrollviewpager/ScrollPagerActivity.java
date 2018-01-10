88631005bf00494c381975b6dae957a3dd146597
zhxh <zhuxiaohui@niuguwang.com>
2018-01-10 18:36:09 +0800

    CYScrollView

--- app/src/main/java/com/android/cy/scrollviewpager/ScrollPagerActivity.java
+++ app/src/main/java/com/android/cy/scrollviewpager/ScrollPagerActivity.java
@@ -18,15 +18,16 @@ import java.util.List;
 public class ScrollPagerActivity extends FragmentActivity implements View.OnClickListener {
 
     CYScrollView scrollView;
-    RadioGroup rg_snapbar_top, rg_snapbar;
-    RadioButton btn_tab1_top, btn_tab2_top, btn_tab3_top, btn_tab1, btn_tab2, btn_tab3;
+    RadioGroup rgTabTopLayout, rgTabBottomLayout;
+    RadioButton btn_tab1_top, btn_tab2_top, btn_tab3_top;
+    RadioButton btn_tab1_bottom, btn_tab2_bottom, btn_tab3_bottom;
 
-    CYViewPager vp;
-    List<View> views;
+    CYViewPager cyViewPager;
+    List<View> viewList;
     MyAdapter myAdapter;
     private LayoutInflater mInflater;
 
-    int snapbar_y;
+    int tabHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,38 +35,38 @@ public class ScrollPagerActivity extends FragmentActivity implements View.OnClic
         setContentView(R.layout.activity_scroll_tab);
         mInflater = LayoutInflater.from(this);
         initView();
-        snapbar_y = dip2px(this, 180);//注意你的snapbar以上部分的高度值，将其转换为px（最好设置为固定值，如果非固定，则要动态计算高度）
+        tabHeight = dip2px(this, 180);//注意你的snapbar以上部分的高度值，将其转换为px（最好设置为固定值，如果非固定，则要动态计算高度）
     }
 
     private void initView() {
         scrollView = (CYScrollView) findViewById(R.id.scrollView);
 
-        rg_snapbar_top = (RadioGroup) findViewById(R.id.rg_snapbar_top);
+        rgTabTopLayout = (RadioGroup) findViewById(R.id.rgTabTopLayout);
         btn_tab1_top = (RadioButton) findViewById(R.id.btn_tab1_top);
         btn_tab2_top = (RadioButton) findViewById(R.id.btn_tab2_top);
         btn_tab3_top = (RadioButton) findViewById(R.id.btn_tab3_top);
 
-        rg_snapbar = (RadioGroup) findViewById(R.id.rg_snapbar);
-        btn_tab1 = (RadioButton) findViewById(R.id.btn_tab1);
-        btn_tab2 = (RadioButton) findViewById(R.id.btn_tab2);
-        btn_tab3 = (RadioButton) findViewById(R.id.btn_tab3);
+        rgTabBottomLayout = (RadioGroup) findViewById(R.id.rgTabBottomLayout);
+        btn_tab1_bottom = (RadioButton) findViewById(R.id.btn_tab1);
+        btn_tab2_bottom = (RadioButton) findViewById(R.id.btn_tab2);
+        btn_tab3_bottom = (RadioButton) findViewById(R.id.btn_tab3);
 
-        btn_tab1.setOnClickListener(this);
+        btn_tab1_bottom.setOnClickListener(this);
         btn_tab1_top.setOnClickListener(this);
-        btn_tab2.setOnClickListener(this);
+        btn_tab2_bottom.setOnClickListener(this);
         btn_tab2_top.setOnClickListener(this);
-        btn_tab3.setOnClickListener(this);
+        btn_tab3_bottom.setOnClickListener(this);
         btn_tab3_top.setOnClickListener(this);
 
-        vp = (CYViewPager) findViewById(R.id.vp);
+        cyViewPager = (CYViewPager) findViewById(R.id.vp);
 
         scrollView.setScrollViewListener(new CYScrollView.ScrollViewListener() {
             @Override
             public void onScrollChanged(CYScrollView scrollView, int x, int y, int oldx, int oldy) {
-                if (y >= snapbar_y) {
-                    rg_snapbar_top.setVisibility(View.VISIBLE);
+                if (y >= tabHeight) {
+                    rgTabTopLayout.setVisibility(View.VISIBLE);
                 } else {
-                    rg_snapbar_top.setVisibility(View.GONE);
+                    rgTabTopLayout.setVisibility(View.GONE);
                 }
             }
         });
@@ -75,21 +76,21 @@ public class ScrollPagerActivity extends FragmentActivity implements View.OnClic
     }
 
     /**
-     * 初始化viewpaper
+     * 初始化viewPaper
      */
     private void initViewPaper() {
-        views = new ArrayList<>();
+        viewList = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             View view = mInflater.inflate(R.layout.item_tab, null);
             LinearLayout ll = (LinearLayout) view.findViewById(R.id.ll);
             for (int j = 0; j < 20; j++) {
                 ll.addView(getView(i));
             }
-            views.add(view);
+            viewList.add(view);
         }
         myAdapter = new MyAdapter();
-        vp.setAdapter(myAdapter);
-        vp.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        cyViewPager.setAdapter(myAdapter);
+        cyViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -99,15 +100,15 @@ public class ScrollPagerActivity extends FragmentActivity implements View.OnClic
             public void onPageSelected(int position) {
                 switch (position) {
                     case 0:
-                        btn_tab1.setChecked(true);
+                        btn_tab1_bottom.setChecked(true);
                         btn_tab1_top.setChecked(true);
                         break;
                     case 1:
-                        btn_tab2.setChecked(true);
+                        btn_tab2_bottom.setChecked(true);
                         btn_tab2_top.setChecked(true);
                         break;
                     case 2:
-                        btn_tab3.setChecked(true);
+                        btn_tab3_bottom.setChecked(true);
                         btn_tab3_top.setChecked(true);
                         break;
                 }
@@ -121,16 +122,11 @@ public class ScrollPagerActivity extends FragmentActivity implements View.OnClic
 
     }
 
-    /**
-     * 填充viewpager的itmeview
-     *
-     * @return
-     */
     private View getView(int i) {
         View view = mInflater.inflate(R.layout.item_scroll_pager, null);
         //填充数据//
         TextView tv_title = (TextView) view.findViewById(R.id.tv_title);
-        tv_title.setText("标题" + (i + 1));
+        tv_title.setText("我是标题" + (i + 1));
         return view;
     }
 
@@ -138,13 +134,13 @@ public class ScrollPagerActivity extends FragmentActivity implements View.OnClic
 
         @Override
         public int getCount() {
-            return views.size();
+            return viewList.size();
         }
 
         @Override
         public Object instantiateItem(View arg0, int arg1) {
-            ((ViewPager) arg0).addView(views.get(arg1));
-            return views.get(arg1);
+            ((ViewPager) arg0).addView(viewList.get(arg1));
+            return viewList.get(arg1);
         }
 
         @Override
@@ -164,15 +160,15 @@ public class ScrollPagerActivity extends FragmentActivity implements View.OnClic
         switch (v.getId()) {
             case R.id.btn_tab1:
             case R.id.btn_tab1_top:
-                vp.setCurrentItem(0);
+                cyViewPager.setCurrentItem(0);
                 break;
             case R.id.btn_tab2:
             case R.id.btn_tab2_top:
-                vp.setCurrentItem(1);
+                cyViewPager.setCurrentItem(1);
                 break;
             case R.id.btn_tab3:
             case R.id.btn_tab3_top:
-                vp.setCurrentItem(2);
+                cyViewPager.setCurrentItem(2);
                 break;
         }
     }
