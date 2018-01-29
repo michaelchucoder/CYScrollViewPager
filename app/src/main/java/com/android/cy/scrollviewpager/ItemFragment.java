1c41379b4b54d02700670f15af59f9a59b12d830
zhxh <zhuxiaohui@niuguwang.com>
2018-01-29 14:56:22 +0800

    ScrollPagerFragActivity

--- app/src/main/java/com/android/cy/scrollviewpager/ItemFragment.java
+++ app/src/main/java/com/android/cy/scrollviewpager/ItemFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,11 +20,14 @@ import com.android.cy.scrollviewpager.dummy.DummyContent;
  */
 public class ItemFragment extends Fragment {
 
+    final static String TAG = "ItemFragment";
+
     LayoutInflater mInflater;
 
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
     private int mColumnCount = 1;
+    private boolean isLoadingMore;
 
     public ItemFragment() {
     }
@@ -59,15 +63,46 @@ public class ItemFragment extends Fragment {
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             RecyclerView recyclerView = (RecyclerView) view;
+
+            RecyclerView.LayoutManager mLayoutManager;
             if (mColumnCount <= 1) {
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                mLayoutManager = new LinearLayoutManager(context);
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+                mLayoutManager = new GridLayoutManager(context, mColumnCount);
             }
             recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS));
             recyclerView.setNestedScrollingEnabled(false);
+
+            final RecyclerView.LayoutManager finalMLayoutManager = mLayoutManager;
+            recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+                    int lastVisibleItem = ((LinearLayoutManager) finalMLayoutManager).findLastVisibleItemPosition();
+                    int totalItemCount = finalMLayoutManager.getItemCount();
+                    //lastVisibleItem >= totalItemCount - 4 表示剩下4个item自动加载，各位自由选择
+                    // dy>0 表示向下滑动
+                    if (lastVisibleItem >= totalItemCount - 4 && dy > 0) {
+                        if (isLoadingMore) {
+                            Log.d(TAG, "ignore manually update!");
+                        } else {
+                            loadPage();//这里多线程也要手动控制isLoadingMore
+                            isLoadingMore = false;
+                        }
+                    }
+                }
+            });
+
         }
         return view;
     }
 
+    private void loadPage() {
+
+        Log.d(TAG, "more!");
+
+
+    }
 }
